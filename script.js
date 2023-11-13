b395cb481ff3716bca355bcec92377425593ffa4
iamshaylajade <shaylajade21@yahoo.com>
2023-11-13 05:52:22 -0500

    index.html changes

--- script.js
+++ script.js
@@ -31,7 +31,8 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
 
+
+  
+  // TODO: Add code to display the current date in the header of the page.
+  setInterval(updateTime, 1000)
